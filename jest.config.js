a9519f9f53022b528a0199f00e9c6a367f66c88e
Козлов Алексей <aekozlo1@mts.ru>
2025-05-29 11:42:27 +0300

    cleaned

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ['<rootDir>/tests/jest/setupTests.js'],
-  moduleNameMapper: {
-    '\\.(css|scss)$': 'identity-obj-proxy'
-  }
-};
--- src/components/app-header/app-header.tsx
+++
@@ -1,9 +0,0 @@
-import { FC } from 'react';
-import { AppHeaderUI } from '@ui';
-import { useSelector } from '../../services/store';
-import { userSelectors } from '../../services/slices/userSlice';
-
-export const AppHeader: FC = () => {
-  const userName = useSelector(userSelectors.getUser)?.name;
-  return <AppHeaderUI userName={userName} />;
-};
